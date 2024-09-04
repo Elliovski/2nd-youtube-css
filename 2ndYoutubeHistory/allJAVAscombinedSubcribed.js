f8beb1e5c9771dd80d49fb4b832d1c3d73a54817
Emil <Emil.mv.2022@gmail.com>
2024-09-04 11:07:21 +0400

    finished css of history youtube

---
+++ 2ndYoutubeHistory/allJAVAscombinedSubcribed.js
@@ -0,0 +1,22 @@
+import { allVideoData } from "../data/allVideoData.js"
+import { renderVideos } from "../youtube2nd javascript/generateVideos.js"
+import { sideBarFunction } from "../youtube2nd javascript/sidebar.js"
+import { genereateChipContent } from "../youtube2nd javascript/genereateChipContent.js"
+import { SubscribedYoutuberVideoList , SubscribedYoutubers} from "../WidelyUsedFunctions/listFunctions.js"
+
+/*
+import { allVideoData } from "../data/allVideoData.js";
+import { renderVideos } from "../youtube2nd javascript/generateVideos.js";
+import { sideBarFunction } from "../youtube2nd javascript/sidebar.js";
+import { genereateChipContent } from "../youtube2nd javascript/genereateChipContent.js";
+*/
+
+
+//genereateChipContent()
+sideBarFunction()
+console.log(SubscribedYoutuberVideoList)
+//renderVideos(SubscribedYoutuberVideoList)
+
+
+
+    
